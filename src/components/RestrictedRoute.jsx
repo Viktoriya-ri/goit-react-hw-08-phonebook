b833fed7a18bd85940f616b8e73bd32eab793ce0
Вікторія <viktoriya23071@gmail.com>
2023-11-11 22:22:44 +0200

    Update RestrictedRoute.jsx

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -1,11 +1,6 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from 'hooks';
 
-/**
- * - If the route is restricted and the user is logged in, render a <Navigate> to redirectTo
- * - Otherwise render the component
- */
-
 // RestrictedRoute - обмежений маршрут
 export const RestrictedRoute = ({ component, redirectTo = '/' }) => {
   const { isLoggedIn } = useAuth();
